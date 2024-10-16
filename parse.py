7e6eb4e8dfc5a506f9949ef251cc9a03e93d4725
invilso <40578604+invilso@users.noreply.github.com>
2024-10-16 03:34:01 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -6,16 +6,24 @@ import re
 
 
 class Parser():
-    servers = ['rpg', 'rp', 'rp2']
+    servers = ['rpg', '1rp', '2rp']
     forum = LoginAndGet()
 
     def parse(self, serv: int):
         server = self.servers[serv]
-        with open(server+'.json', "w+", encoding="utf-8") as f:
-            if server == 'rp2':
-                f.write(json.dumps(re.split('<br>', self.forum.getF("http://gta-trinity.ru/"+server+"mon/bans.php", False))[4::], sort_keys=True, indent=4))
-            else:
-                f.write(json.dumps(re.split('<br>', self.forum.getF("http://gta-trinity.ru/"+server+"mon/bans.php", False))[5::], sort_keys=True, indent=4))
+        # Новий URL для API запиту
+        url = f"https://gta-trinity.com/forum/api/monitoring/?columns[0][search][regex]=false&columns[1][data]=1&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=false&columns[1][search][value]=&columns[1][search][regex]=false&order[0][column]=0&order[0][dir]=desc&start=0&length=200&search[value]=54345&search[regex]=true&monitoring={serv}ban"
+        response = self.forum.getF(url, False)
+        
+        # Парсимо відповідь як JSON
+        data = json.loads(response)
+        
+        # Витягуємо лише банлисти без айдішників (тільки другі елементи масиву)
+        banlist = [entry[1] for entry in data.get("data", [])]
+
+        # Записуємо банлист у файл
+        with open(f'{server}.json', "w+", encoding="utf-8") as f:
+            json.dump(banlist, f, ensure_ascii=False, indent=4)
 
 # p = Parser()
 # while True:
